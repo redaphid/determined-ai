ba6108e4151ba1287a4fb6da9fcd8d5b8d02c821
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2023-01-12 14:48:53 -0500

    feat: strategic merge pod spec with task_container_defaults

--- master/pkg/model/task_container_defaults_test.go
+++ master/pkg/model/task_container_defaults_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	k8sV1 "k8s.io/api/core/v1"
+
 	"github.com/determined-ai/determined/master/pkg/schemas/expconf"
 )
 
@@ -44,4 +46,59 @@ func TestEnvironmentVarsDefaultMerging(t *testing.T) {
 
 	require.Equal(t, *conf.RawSlurmConfig.RawGpuType, gpuType)
 	require.Equal(t, *conf.RawPbsConfig.RawSlotsPerNode, pbsSlotsPerNode)
+}
+
+func TestPodSpecsDefaultMerging(t *testing.T) {
+	defaults := &TaskContainerDefaultsConfig{
+		CPUPodSpec: &k8sV1.Pod{
+			Spec: k8sV1.PodSpec{
+				SecurityContext: &k8sV1.PodSecurityContext{
+					SELinuxOptions: &k8sV1.SELinuxOptions{
+						Level: "cpuLevel",
+						Role:  "cpuRole",
+					},
+				},
+			},
+		},
+		GPUPodSpec: &k8sV1.Pod{
+			Spec: k8sV1.PodSpec{
+				SecurityContext: &k8sV1.PodSecurityContext{
+					SELinuxOptions: &k8sV1.SELinuxOptions{
+						Level: "gpuLevel",
+						Role:  "gpuRole",
+					},
+				},
+			},
+		},
+	}
+
+	for i := 0; i <= 1; i++ {
+		conf := expconf.ExperimentConfig{
+			RawResources: &expconf.ResourcesConfig{RawSlotsPerTrial: &i},
+			RawEnvironment: &expconf.EnvironmentConfig{
+				RawPodSpec: &expconf.PodSpec{
+					Spec: k8sV1.PodSpec{
+						SecurityContext: &k8sV1.PodSecurityContext{
+							SELinuxOptions: &k8sV1.SELinuxOptions{
+								Level: "expconfLevel",
+							},
+						},
+					},
+				},
+			},
+		}
+		defaults.MergeIntoExpConfig(&conf)
+
+		expected := &expconf.PodSpec{
+			Spec: k8sV1.PodSpec{
+				SecurityContext: &k8sV1.PodSecurityContext{
+					SELinuxOptions: &k8sV1.SELinuxOptions{
+						Level: "expconfLevel",
+						Role:  []string{"cpuRole", "gpuRole"}[i],
+					},
+				},
+			},
+		}
+		require.Equal(t, expected, conf.RawEnvironment.RawPodSpec)
+	}
 }
