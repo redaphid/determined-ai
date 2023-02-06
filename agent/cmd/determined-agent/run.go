determined-ai/determined

refactor: rewrite agent without actors

agent/cmd/determined-agent/run.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
 	"os"
-	"time"
 
 	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/determined-ai/determined/agent/internal"
+	"github.com/determined-ai/determined/agent/internal/options"
 	"github.com/determined-ai/determined/master/pkg/aproto"
 	"github.com/determined-ai/determined/master/pkg/check"
 )
@@ -26,7 +27,7 @@ func readConfigFile(configPath string) ([]byte, error) {
 	var err error
 	if _, err = os.Stat(configPath); err != nil {
 		if isDefault && os.IsNotExist(err) {
-			logrus.Warnf("no configuration file at %s, skipping", configPath)
+			log.Warnf("no configuration file at %s, skipping", configPath)
 			return nil, nil
 		}
 		return nil, errors.Wrap(err, "error finding configuration file")
@@ -39,7 +40,7 @@ func readConfigFile(configPath string) ([]byte, error) {
 }
 
 func newRunCmd() *cobra.Command {
-	opts := internal.Options{}
+	opts := options.AgentOptions{}
 
 	cmd := &cobra.Command{
 		Use:   "run",
@@ -71,8 +72,10 @@ func newRunCmd() *cobra.Command {
 			return errors.Wrap(err, "command-line arguments specify illegal configuration")
 		}
 
-		if err := internal.Run(version, opts); err != nil {
-			logrus.Fatal(err)
+		opts.Resolve()
+
+		if err := internal.Run(context.Background(), version, opts); err != nil {
+			log.Fatal(err)
 		}
 
 		return nil
@@ -151,11 +154,6 @@ func newRunCmd() *cobra.Command {
 		"TCP port for the Fluent Bit daemon to listen on")
 	cmd.Flags().StringVar(&opts.Fluent.ContainerName, "fluent-container-name", "determined-fluent",
 		"Name for the Fluent Bit container")
-
-	cmd.Flags().IntVar(&opts.AgentReconnectAttempts, "agent-reconnect-attempts",
-		aproto.AgentReconnectAttempts, "Max attempts agent has to reconnect")
-	cmd.Flags().IntVar(&opts.AgentReconnectBackoff, "agent-reconnect-backoff",
-		int(aproto.AgentReconnectBackoff/time.Second), "Time between agent reconnect attempts")
 
 	return cmd
 }

agent/internal/api.go
@@ -1,0 +1,57 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+
+	"github.com/determined-ai/determined/agent/internal/options"
+	"github.com/determined-ai/determined/master/pkg/logger"
+)
+
+type agentAPIServer struct {
+	// Configuration details.
+	opts options.AgentOptions
+
+	// Internal state.
+	server *echo.Echo
+}
+
+func newAgentAPIServer(opts options.AgentOptions) *agentAPIServer {
+	server := echo.New()
+	server.Logger = logger.New()
+	server.HidePort = true
+	server.HideBanner = true
+	server.Use(middleware.Recover())
+	server.Pre(middleware.RemoveTrailingSlash())
+	server.Use(otelecho.Middleware("determined-agent"))
+
+	server.Any("/debug/pprof/*", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
+	server.Any("/debug/pprof/cmdline", echo.WrapHandler(http.HandlerFunc(pprof.Cmdline)))
+	server.Any("/debug/pprof/profile", echo.WrapHandler(http.HandlerFunc(pprof.Profile)))
+	server.Any("/debug/pprof/symbol", echo.WrapHandler(http.HandlerFunc(pprof.Symbol)))
+	server.Any("/debug/pprof/trace", echo.WrapHandler(http.HandlerFunc(pprof.Trace)))
+
+	return &agentAPIServer{
+		opts:   opts,
+		server: server,
+	}
+}
+
+func (a *agentAPIServer) serve() error {
+	bindAddr := fmt.Sprintf("%s:%d", a.opts.BindIP, a.opts.BindPort)
+	logrus.Infof("starting agent server on [%s]", bindAddr)
+	if a.opts.TLS {
+		return a.server.StartTLS(bindAddr, a.opts.CertFile, a.opts.KeyFile)
+	}
+	return a.server.Start(bindAddr)
+}
+
+func (a *agentAPIServer) close() error {
+	return a.server.Close()
+}

redaphid/determined-ai

agent/cmd/determined-agent/run.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"io/ioutil"
 	"os"
 	"time"
 
 	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 
 	"github.com/determined-ai/determined/agent/internal"
+	"github.com/determined-ai/determined/agent/internal/options"
 	"github.com/determined-ai/determined/master/pkg/aproto"
 	"github.com/determined-ai/determined/master/pkg/check"
 )
@@ -26,7 +28,7 @@ func readConfigFile(configPath string) ([]byte, error) {
 	var err error
 	if _, err = os.Stat(configPath); err != nil {
 		if isDefault && os.IsNotExist(err) {
-			logrus.Warnf("no configuration file at %s, skipping", configPath)
+			log.Warnf("no configuration file at %s, skipping", configPath)
 			return nil, nil
 		}
 		return nil, errors.Wrap(err, "error finding configuration file")
@@ -39,7 +41,7 @@ func readConfigFile(configPath string) ([]byte, error) {
 }
 
 func newRunCmd() *cobra.Command {
-	opts := internal.Options{}
+	opts := options.Options{}
 
 	cmd := &cobra.Command{
 		Use:   "run",
@@ -71,8 +73,10 @@ func newRunCmd() *cobra.Command {
 			return errors.Wrap(err, "command-line arguments specify illegal configuration")
 		}
 
-		if err := internal.Run(version, opts); err != nil {
-			logrus.Fatal(err)
+		opts.Resolve()
+
+		if err := internal.Run(context.Background(), version, opts); err != nil {
+			log.Fatal(err)
 		}
 
 		return nil
@@ -152,6 +156,7 @@ func newRunCmd() *cobra.Command {
 	cmd.Flags().StringVar(&opts.Fluent.ContainerName, "fluent-container-name", "determined-fluent",
 		"Name for the Fluent Bit container")
 
+	// Fault-tolerance flags.
 	cmd.Flags().IntVar(&opts.AgentReconnectAttempts, "agent-reconnect-attempts",
 		aproto.AgentReconnectAttempts, "Max attempts agent has to reconnect")
 	cmd.Flags().IntVar(&opts.AgentReconnectBackoff, "agent-reconnect-backoff",

agent/internal/api.go
@@ -1,0 +1,57 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+
+	"github.com/determined-ai/determined/agent/internal/options"
+	"github.com/determined-ai/determined/master/pkg/logger"
+)
+
+type agentAPIServer struct {
+	// Configuration details.
+	opts options.Options
+
+	// Internal state.
+	server *echo.Echo
+}
+
+func newAgentAPIServer(opts options.Options) *agentAPIServer {
+	server := echo.New()
+	server.Logger = logger.New()
+	server.HidePort = true
+	server.HideBanner = true
+	server.Use(middleware.Recover())
+	server.Pre(middleware.RemoveTrailingSlash())
+	server.Use(otelecho.Middleware("determined-agent"))
+
+	server.Any("/debug/pprof/*", echo.WrapHandler(http.HandlerFunc(pprof.Index)))
+	server.Any("/debug/pprof/cmdline", echo.WrapHandler(http.HandlerFunc(pprof.Cmdline)))
+	server.Any("/debug/pprof/profile", echo.WrapHandler(http.HandlerFunc(pprof.Profile)))
+	server.Any("/debug/pprof/symbol", echo.WrapHandler(http.HandlerFunc(pprof.Symbol)))
+	server.Any("/debug/pprof/trace", echo.WrapHandler(http.HandlerFunc(pprof.Trace)))
+
+	return &agentAPIServer{
+		opts:   opts,
+		server: server,
+	}
+}
+
+func (a *agentAPIServer) serve() error {
+	bindAddr := fmt.Sprintf("%s:%d", a.opts.BindIP, a.opts.BindPort)
+	logrus.Infof("starting agent server on [%s]", bindAddr)
+	if a.opts.TLS {
+		return a.server.StartTLS(bindAddr, a.opts.CertFile, a.opts.KeyFile)
+	}
+	return a.server.Start(bindAddr)
+}
+
+func (a *agentAPIServer) close() error {
+	return a.server.Close()
+}